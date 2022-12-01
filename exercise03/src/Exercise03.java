2e057e4670e2313bd35ab1b4ff5a91619a581e41
Pabel Hernandez <pabel.hernandez@live.com>
2022-12-01 16:23:11 -0600

    exercise03
    E3 upload

---
+++ exercise03/src/Exercise03.java
@@ -0,0 +1,11 @@
+public class Exercise03 {
+    public static void main(String[] args) {
+
+        String[] txt = new String[]{"Example Text 1. ", "Example Text 2. ", "Example Text 3. "};
+
+
+        for(int i = 0; i < txt.length; i++) {
+            System.out.print(txt[i]);
+        }
+    }
+}
